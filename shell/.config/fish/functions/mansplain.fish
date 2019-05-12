a0215f261935dfdbfe4fb3e6a4924f24f744dec9
Evan Harvey <harveyem@utas.edu.au>
2019-05-13 04:29:08 +1000

    Fixed awk

--- shell/.config/fish/functions/mansplain.fish
+++ shell/.config/fish/functions/mansplain.fish
@@ -1,3 +1,12 @@
-function mansplain --description 'Like man, but actually an expert'
-	man -k . | dmenu -l 30 | awk '{print }' | xargs -r man -Tpdf | zathura -
+# Non-POSIX dependencies: 
+# dmenu
+# zathura
+# zathura-pdf-mupdf
+function mansplain --description 'Think you\'re an expert? Mansplain knows better.'
+	set options 30 # Arbitrary, the # of options for dmenu to show at a time
+	man -k . |\
+	dmenu -l $options |\
+	awk '{print $1}' |\
+	xargs -r man -Tpdf |\
+	zathura -
 end
