a697c1c1f7cde08d641cf071cdb9ea7c7f9ad55b
Evan Harvey <harveyem@utas.edu.au>
2019-09-14 11:31:59 +1000

    Fix i3 environment 'expansion'

---
+++ shell/.config/fish/functions/fzf_custom.fish
@@ -0,0 +1,19 @@
+function fzf_uni
+	cd ~/Uni/ && exec fzf
+end
+
+function fzf_dev
+	cd ~/dev/ && exec fzf
+end
+
+function fzf_vimwiki
+	cd ~/Dropbox/vimwiki && exec fzf
+end
+
+function fzf_pictures
+	cd ~/pictures/ && exec fzf
+end
+
+function fzf_downloads
+	cd ~/downloads && exec fzf
+end
